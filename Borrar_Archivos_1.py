859da6355e0458703e8af67c33a24e73309d00e9
ryooxnay <87193216+ryooxnay@users.noreply.github.com>
2021-11-14 19:28:12 -0600

    Create Borrar_Archivos_1.py

---
+++ Borrar_Archivos_1.py
@@ -0,0 +1,10 @@
+import os
+
+# if os.path.exists("demo3.txt"):
+#     os.remove("demo3.txt")
+#     print("el archivo ist kaput")
+# else:
+#     print("arhivo no encontrado")
+
+# os.mkdir("test")
+os.rmdir("test")
