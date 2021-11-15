b290391d3422016eb01ca4d2d1b0b6cc250cbed2
ryooxnay <87193216+ryooxnay@users.noreply.github.com>
2021-11-14 19:27:39 -0600

    Create Escribir_Archivos_1.py

---
+++ Escribir_Archivos_1.py
@@ -0,0 +1,16 @@
+file = "demo4.txt"
+
+f = open(file, "wt")
+f.close()
+
+# f = open(file, "rt")
+# print(f.read())
+# f.close()
+
+# f = open(file, "at")
+# f.write("---- fin de archivo -----")
+# f.close()
+
+# f = open(file, "rt")
+# print(f.read())
+# f.close()
